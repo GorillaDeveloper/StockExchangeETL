469582f3ecd53a72ec1a4febccfa325e8f3fd87b
GorillaDevelopers <ghz.freelancer92@gmail.com>
2023-06-06 23:19:44 +0500

    fixing: folder creation path

--- FileExtractor.py
+++ FileExtractor.py
@@ -47,6 +47,7 @@ def for_linux(file_path,output_directory,current_date):
 
         print('i am trying to create folder at '+temp_folder_name+' and my current working directory is '+os.getcwd())
         print('output directory: '+output_directory)
+        
         tt =  str(current_date)+'.lis'
         lis_file_name = file_path.replace('.Z','')
         print(f'mv {lis_file_name} {os.path.join(output_directory, tt)}')
@@ -55,7 +56,7 @@ def for_linux(file_path,output_directory,current_date):
         # subprocess.run(f"mv {temp_file_path} {temp_folder_name}/",shell=True )
         # zip_file_path = os.path.join(temp_folder_name, temo_dot_Z_file_name[0])
         subprocess.run(["gzip", "-d", ".Z", file_path])
-        subprocess.run(["mv ","-f", lis_file_name,tt])
+        subprocess.run(["mv ","-f", temo_dot_Z_file_name,tt])
         
         # os.rename(f"{temp_folder_name}/{lis_file_name}", f"{temp_folder_name}/{current_date}.lis")
         
